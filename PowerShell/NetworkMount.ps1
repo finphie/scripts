319aa82c112a45728b9c0b1f552d13daf8f612f2
finphie <19237170+finphie@users.noreply.github.com>
2017-01-15 19:58:00 +0900

    ネットワークドライブの割り当てをするスクリプトを追加

---
+++ PowerShell/NetworkMount.ps1
@@ -0,0 +1,36 @@
+# ネットワークドライブ割り当て
+
+<#
+    「使い方」
+    1. $path, $drive, $user, $fileを設定する。
+    2. PowerShellを実行すると、ネットワークフォルダを割り当てます。
+    3. もう一度実行すると、割り当てを解除します。
+#>
+
+# ネットワークフォルダ（\\NAS\Shareなど）
+$path = ""
+# ドライブ名（Zなど）
+$drive = "Z"
+# ユーザー名
+$user = "user"
+# MakePasswordFile.ps1で作成したパスワードファイルのパス
+$file = "password.txt"
+
+
+$dir = Split-Path $MyInvocation.MyCommand.Path -parent
+$file = Join-Path $dir $file
+$driveName = $drive + ":"
+
+if (Get-PSDrive | where {$_.Name -eq $drive})
+{
+    net use $driveName /delete
+}
+else
+{
+    $secure = Get-Content $file | ConvertTo-SecureString
+    $cred = New-Object System.Management.Automation.PSCredential $user,$secure
+
+    New-PSDrive -Persist -Scope "Global" -Name $drive -PSProvider FileSystem -Root $path -Credential $cred
+    
+    Invoke-Item $driveName
+}
