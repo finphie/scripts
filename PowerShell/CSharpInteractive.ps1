37a653f32a0282b2b1e9896c699bc571a0892c43
finphie <19237170+finphie@users.noreply.github.com>
2017-01-16 20:19:46 +0900

    C#スクリプトを実行するPowerShellスクリプトを追加

---
+++ PowerShell/CSharpInteractive.ps1
@@ -0,0 +1,18 @@
+# C#スクリプトを実行する
+<#
+    「使い方」
+    引数に実行したいC#スクリプトのパスを渡してください。
+#>
+
+Param($path)
+
+$path = [System.IO.Path]::GetFullPath($path)
+
+$csiPath = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions" |
+    Sort-Object {[double]$_.PSChildName} -Descending |
+    Select-Object -First 1 |
+    Get-ItemProperty -Name MSBuildToolsPath |
+    Select -ExpandProperty MSBuildToolsPath
+
+Set-Location $csiPath
+Invoke-Expression ".\csi.exe $path"
