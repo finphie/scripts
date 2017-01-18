8396bcc90577b8f05ff8c9520153256b99489496
finphie <19237170+finphie@users.noreply.github.com>
2017-01-18 12:05:36 +0900

    ショートカット作成スクリプトを追加

---
+++ PowerShell/CreateShortcut.ps1
@@ -0,0 +1,28 @@
+﻿# ショートカット作成
+<#
+    「使い方」
+    引数にファイルパスを指定すると、そのファイルのショートカットを作成します。
+#>
+
+Param($path)
+
+$name = (Read-Host "ショートカット名") + ".lnk"
+$desktop = [Environment]::GetFolderPath([Environment+SpecialFolder]::DesktopDirectory)
+$shortcutPath = Join-Path $desktop $name
+
+$extension = (Get-Item $path).Extension.Substring(1)
+switch ($extension)
+{
+    "ps1" { $arguments = "-File ""$path"""; $path = "powershell" }
+}
+
+$wsh = New-Object -ComObject WScript.Shell
+$shortcut = $wsh.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = $path
+if ($arguments) 
+{
+    $shortcut.Arguments = $arguments
+}
+$shortcut.Save()
+
+[Runtime.InteropServices.Marshal]::FinalReleaseComObject($wsh) | Out-Null
